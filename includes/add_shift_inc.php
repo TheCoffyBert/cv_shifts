4a4415ed65fa24975fd40d21dafb56db3c8432ea
Paolo Bertinelli <paolo.bertinelli@gmail.com>
2019-03-09 10:38:43 +0100

    Update add_shift_inc.php
    Now the service description is initialized to NULL if there isn't any service

--- includes/add_shift_inc.php
+++ includes/add_shift_inc.php
@@ -6,12 +6,12 @@ if (isset($_POST['add_shift-submit'])){
     //Manages the connection between the website and the DB. More info in the file itself
     require 'db_handler_inc.php';
 
-    //Storing info from the "new shift" form
+    //Storing info from the "add_shift" form
     $start = $_POST['start'];
     $end = $_POST['end'];
-    //I need to store the difference between the two timestamps in seconds in order to calculate how much time I spend there
+    //  I need to store the difference between the two timestamps in seconds in order to calculate how much time I spend there
     $time = strtotime($end) - strtotime($start);
-    //From here, every variable equal to 0 will be stored as NULL, only because it'll make the table look better (and I don't need any of those data anyway...)
+    //  The add_shift form stores unwritten values as 0, so this part sets them to NULL
     $emergencies = $_POST['emergencies'];
     if ($emergencies == 0){
         $emergencies = NULL;
@@ -24,17 +24,18 @@ if (isset($_POST['add_shift-submit'])){
     if ($services == 0){
         $services = NULL;
     }
-    //TODO: Actually use this variable. At this moment it's useless
     $service_description = $_POST['service_description'];
+    if ($service_description == ''){
+        $service_description = NULL;
+    }
 
-    /*
-     * FORM VALIDATION:
-     * In this part of the code I validate every field and, if the program finds some error, send the
-     * user back to the index page* with a proper warning. I'm planning to check if:
-     *  - The user didn't fill the start and end fields (done);
-     *  - The user didn't write a service description;
-     *
-     *   *I'm wondering if it's better to send the user back to the "new shift" page. I'll test it in the next couple of days
+    /* FORM VALIDATION:
+     * In this part of the code I validate every field and, if the program finds some error, send the user back to the
+     * add_shift page with a proper warning, otherwhise it sends the user back to the main page. So far, the program
+     * checks for:
+     *  - Empty start or end fields;
+     *  - Empty service description if there's at least a service;
+     * - TODO: There's already a service that matches both start and end time
     */
 
     if (empty($start) || empty($end)){
